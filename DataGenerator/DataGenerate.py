926eb0b171404273b0154df1d927609630c06627
Prasanna C <93747727+PrasannaJC@users.noreply.github.com>
2023-02-27 19:08:17 -0600

    Fix to DataGenerate

--- DataGenerator/DataGenerate.py
+++ DataGenerator/DataGenerate.py
@@ -24,18 +24,18 @@ dbp = []
 rr = []
 Temperature = []
 
-heartRange = []
+hRange = []
 sysRange = []
 
 for i in range(85, 205):
     if i <= 110:
         for k in range(6):
-            heartRange.append(i)
+            hRange.append(i)
     elif 110 < i <= 140:
         for k in range(3):
-            heartRange.append(i)
+            hRange.append(i)
     elif 140 < i:
-        heartRange.append(i)
+        hRange.append(i)
 for i in range(85, 165):
     if i <= 120:
         for k in range(6):
@@ -49,18 +49,18 @@ for i in range(85, 165):
 
 for i in range(500):
     G.append(rand.choice(["M", "F"]))
-    Age.append(npy.random.randint(21, 69))
-    BloodOxy.append(npy.random.randint(92, 100))
+    Age.append(rand.randint(21, 69))
+    BloodOxy.append(rand.randint(92, 100))
 
     # Heartrate
     if 21 <= Age[i] <= 36:
-        HeartRate.append(npy.random.randint(heartRange))
+        HeartRate.append(rand.choice(hRange))
     elif 37 <= Age[i] <= 49:
-        HeartRate.append(npy.random.randint(heartRange))
-    elif 50 <= Age[i] <= 68:
-        HeartRate.append(npy.random.randint(heartRange))
+        HeartRate.append(rand.choice(hRange))
+    elif 50 <= Age[i] <= 69:
+        HeartRate.append(rand.choice(hRange))
     # Blood pressure - both systolic and diastolic
-    sbp.append(npy.random.randint(sysRange))
+    sbp.append(rand.choice(sysRange))
 
     if 21 <= Age[i] <= 40:
         if 85 <= sbp[i] <= 125:
@@ -76,7 +76,7 @@ for i in range(500):
             dbp.append(npy.random.randint(74, 86))
         else:
             dbp.append(npy.random.randint(87, 99))
-    elif 57 <= Age[i] <= 68:
+    elif 57 <= Age[i] <= 69:
         if sbp[i] <= 119:
             dbp.append(npy.random.randint(63, 71))
         elif 120 <= sbp[i] <= 138:
@@ -85,7 +85,7 @@ for i in range(500):
             dbp.append(npy.random.randint(84, 96))
 
     rr.append(npy.random.randint(5, 28))
-    Temperature.append(round(npy.random.uniform(96.51, 103.19), 2))
+    Temperature.append(round(npy.random.uniform(96.78, 103.19), 2))
 
 BodyStats = {
     'Gender': G,
@@ -105,7 +105,6 @@ print(len(sbp))
 print(len(dbp))
 print(len(rr))
 print(len(Temperature))
-
 
 df = pds.DataFrame(BodyStats)
 
