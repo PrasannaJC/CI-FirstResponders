c3d32cbfc315c600181e081f752dd7e64cd7cf1f
elopezbanderas <60753149+elopezbanderas@users.noreply.github.com>
2023-03-19 18:20:46 -0500

    Implemented models: Event, Responds

---
+++ MonitoringSuiteLibrary/MonitoringSuiteLibrary/Models/Event.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonitoringSuiteLibrary.Models
+{
+
+    /// <summary>
+    /// The struct that represents vitals.
+    /// </summary>
+    public struct Event
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Creates an Event object
+        /// </summary>
+        /// <
+        /// <param name="e_id">The Event's unique id.</param>
+        /// <param name="location">The location of event.</param>
+        /// <param name="stime">The start time of event.</param>
+        /// <param name="etime">The end time of event. If null, then event is still active.</param>
+        public Event(int e_id, string location, DateTime stime, DateTime etime)
+        {
+            this.EventId = e_id;
+            this.Location = location;
+            this.StartTime = stime;
+            this.EndTime = etime;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the event's unique id.
+        /// </summary>
+        public int EventId { get; }
+
+        /// <summary>
+        /// Gets or sets the event's location.
+        /// </summary>
+        public string Location { get; set; }
+
+        /// <summary>
+        /// Gets or sets the event's start time.
+        /// </summary>
+        public DateTime StartTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the event's end time.
+        /// </summary>
+        public DateTime EndTime { get; set; }
+
+        #endregion
+    }
+}
---
+++ MonitoringSuiteLibrary/MonitoringSuiteLibrary/Models/Responds.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonitoringSuiteLibrary.Models
+{
+
+    /// <summary>
+    /// The struct that represents vitals.
+    /// </summary>
+    public struct Responds
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Creates a Responds object
+        /// </summary>
+        /// <
+        /// <param name="eventObject">The corresponding event <see cref="MontioringSuiteLibrary.Models.Event"/>.</param>
+        /// <param name="firstResponders">The list of first responders that responded the event <see cref="MontioringSuiteLibrary.Models.FirstResponder"/>.</param>
+        public Responds(Event eventObject, List<FirstResponder> firstResponders)
+        {
+            this.Event = eventObject;
+            this.FirstResponders = firstResponders;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the Event object.
+        /// </summary>
+        public Event Event { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of first responders.
+        /// </summary>
+        public List<FirstResponder> FirstResponders { get; set; }
+
+        #endregion
+    }
+}
