9a6a672cb131b4a5b69f9c3ce30cdd433da31fa7
Travis <tjmunyer@gmail.com>
2023-03-19 19:57:33 -0500

    Updated prototypes for the DataService.

--- MonitoringSuiteLibrary/MonitoringSuiteLibrary/Contracts/Services/IDataService.cs
+++ MonitoringSuiteLibrary/MonitoringSuiteLibrary/Contracts/Services/IDataService.cs
@@ -10,28 +10,67 @@ namespace MonitoringSuiteLibrary.Contracts.Services
     /// </summary>
     public interface IDataService
     {
+        #region Public Methods
+
         /// <summary>
         /// Gets a <see cref="IEnumerable{FirstResponder}"/> representing the first responders.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>An IEnumerable of all first responders.</returns>
         public IEnumerable<FirstResponder> GetFirstResponders();
 
         /// <summary>
-        /// Updates an existing first responder.
+        /// Gets a <see cref="IEnumerable{FirstResponder}"/> respresenting the first responders for an event.
         /// </summary>
-        /// <param name="firstResponder">The first responder to update.</param>
-        public void UpdateFirstResponder(FirstResponder firstResponder);
+        /// <param name="eventId">The id corresponding to the specific <see cref="Event"/>.</param>
+        /// <returns>An IEnumerable of first responders corresponding to a specific event.</returns>
+        public IEnumerable<FirstResponder> GetFirstResponders(int eventId);
 
         /// <summary>
-        /// Deletes an existing first responder.
+        /// Gets a <see cref="FirstResponder"/>s vitals.
         /// </summary>
-        /// <param name="firstResponderId">The identifier of the first responder to delete.</param>
-        public void DeleteFirstResponder(int firstResponderId);
+        /// <param name="firstResponderId">The id of the target first responder.</param>
+        /// <returns>The current <see cref="Vitals"/> corresponding to the first responder.</returns>
+        public Vitals GetFirstResponderVitals(int firstResponderId);
 
         /// <summary>
-        /// Adds a first responder.
+        /// Gets a <see cref="FirstResponder"/>s location.
         /// </summary>
-        /// <param name="firstResponder">The <see cref="FirstResponder"/> to add.</param>
-        public void AddFirstResponder(FirstResponder firstResponder);
+        /// <param name="firstResponderId">The id of the target first responder.</param>
+        /// <returns>The current <see cref="Location"/> corresponding to the first responder.</returns>
+        public Location GetFirstResponderLocation(int firstResponderId);
+
+        /// <summary>
+        /// Sets a first responder as inactive.
+        /// </summary>
+        /// <param name="firstResponderId">The id of the first responder to set inactive.</param>
+        /// <returns>Whether or not setting the first responder as inactive was successful.</returns>
+        public bool SetFirstResponderInactive(int firstResponderId);
+
+        /// <summary>
+        /// Sets a first responder as active and initializes location and vitals data.
+        /// </summary>
+        /// <param name="firstResponderId">The id of the target first responder.</param>
+        /// <param name="vitals">The initial vitals data to store for the first responder.</param>
+        /// <param name="location">The initial location data to store for the first responder.</param>
+        /// <returns>Whether or not setting the first responder as active was successful.</returns>
+        public bool SetFirstResponderActive(int firstResponderId, Vitals vitals, Location location);
+
+        /// <summary>
+        /// Updates first responder location.
+        /// </summary>
+        /// <param name="firstResponderId">The id of the first responder to update.</param>
+        /// <param name="location">The <see cref="Location"/> data the current <see cref="Location"/> is updated to.</param>
+        /// <returns>Whether or not the update was successful.</returns>
+        public bool UpdateFirstResponderLocation(int firstResponderId, Location location);
+
+        /// <summary>
+        /// Updates first responder vitals.
+        /// </summary>
+        /// <param name="firstResponderId">The id of the first responder to update.</param>
+        /// <param name="vitals">The <see cref="Vitals"/> data the current <see cref="Vitals"/> is updated to.</param>
+        /// <returns>Whether or not the update was successful.</returns>
+        public bool UpdateFirstResponderVitals(int firstResponderId, Vitals vitals);
+
+        #endregion
     }
 }
--- MonitoringSuiteLibrary/MonitoringSuiteLibrary/Services/DataService.cs
+++ MonitoringSuiteLibrary/MonitoringSuiteLibrary/Services/DataService.cs
@@ -12,12 +12,22 @@ namespace MonitoringSuiteLibrary.Services
     /// </summary>
     public class DataService : IDataService
     {
+        #region Private Fields 
+
         private readonly IOptions<DataServiceConfiguration> _options;
+
+        #endregion
+
+        #region Constructors
 
         public DataService(IOptions<DataServiceConfiguration> options)
         {
             _options = options;
         }
+
+        #endregion
+
+        #region Public Methods
 
         /// <summary>
         /// TODO: Add description and implement <see cref="GetFirstResponders"/>.
@@ -34,33 +44,87 @@ namespace MonitoringSuiteLibrary.Services
         }
 
         /// <summary>
-        /// TODO: Add description and implement <see cref="AddFirstResponder(FirstResponder)"/>.
+        /// TODO: Add description and implement <see cref="GetFirstResponders(int)"/>.
         /// </summary>
-        /// <param name="firstResponder"></param>
+        /// <param name="eventId"></param>
+        /// <returns></returns>
         /// <exception cref="NotImplementedException"></exception>
-        public void AddFirstResponder(FirstResponder firstResponder)
+        public IEnumerable<FirstResponder> GetFirstResponders(int eventId)
+        {
+            // TODO: We may need to be able to GetFirstResponders for a specific event.
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// TODO: Add description and implement <see cref="GetFirstResponderVitals(int)"/>.
+        /// </summary>
+        /// <param name="firstResponderId"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Vitals GetFirstResponderVitals(int firstResponderId)
         {
             throw new NotImplementedException();
         }
 
         /// <summary>
-        /// TODO: Add description and implement <see cref="DeleteFirstResponder(FirstResponder)"/>.
+        /// TODO: Add description and implement <see cref="GetFirstResponderLocation(int)"/>.
         /// </summary>
-        /// <param name="firstResponder"></param>
+        /// <param name="firstResponderId"></param>
+        /// <returns></returns>
         /// <exception cref="NotImplementedException"></exception>
-        public void DeleteFirstResponder(int firstResponderId)
+        public Location GetFirstResponderLocation(int firstResponderId)
         {
             throw new NotImplementedException();
         }
 
         /// <summary>
-        /// TODO: Add description and implement <see cref="UpdateFirstResponder(FirstResponder)"/>.
+        /// TODO: Add description and implement <see cref="SetFirstResponderInactive(int)"/>
         /// </summary>
-        /// <param name="firstResponder"></param>
+        /// <param name="firstResponderId"></param>
+        /// <returns>Whether or not setting the first responder as inactive was successful.</returns>
+        public bool SetFirstResponderInactive(int firstResponderId)
+        {
+            // TODO: Should somehow clear out the vitals and location information. 
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// TODO: Add description and implement <see cref="SetFirstResponderActive(int, Vitals, Location)"/>.
+        /// </summary>
+        /// <param name="firstResponderId">The id of the target first responder.</param>
+        /// <param name="vitals">The initial vitals data to store for the first responder.</param>
+        /// <param name="location">The initial location data to store for the first responder.</param>
+        /// <returns>Whether or not setting the first responder as active was successful.</returns>
         /// <exception cref="NotImplementedException"></exception>
-        public void UpdateFirstResponder(FirstResponder firstResponder)
+        public bool SetFirstResponderActive(int firstResponderId, Vitals vitals, Location location)
         {
             throw new NotImplementedException();
         }
+
+        /// <summary>
+        /// TODO: Add description and implement <see cref="UpdateFirstResponderLocation(int)"/>.
+        /// </summary>
+        /// <param name="firstResponderId"></param>
+        /// <param name="location"></param>
+        /// <returns>Whether or not the update was successful.</returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public bool UpdateFirstResponderLocation(int firstResponderId, Location location)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// TODO: Add description and implement <see cref="UpdateFirstResponderVitals(int)"/>.
+        /// </summary>
+        /// <param name="firstResponderId"></param>
+        /// <param name="vitals"></param>
+        /// <returns>Whether or not the update was successful.</returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public bool UpdateFirstResponderVitals(int firstResponderId, Vitals vitals)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
     }
 }
