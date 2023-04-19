8afc963c76c106082398b970898861f0513c55dd
Prasanna C <93747727+PrasannaJC@users.noreply.github.com>
2023-04-18 23:07:16 -0500

    Update to resolve naming convention for ML folder & file

---
+++ MonitoringSuiteLibrary/MonitoringSuiteLibrary/MachineLearning/CheckDistress.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static MonitoringSuiteLibrary.MachineLearning.checkDistress;
+using static MonitoringSuiteLibrary.Distress;
+using MonitoringSuiteLibrary.Models;
+
+namespace MonitoringSuiteLibrary.MachineLearning
+{
+    public class checkDistress
+    {
+
+        /// <summary>
+        /// Check the distress status of a First Responder object using it's age, gender, and vitals data.
+        /// </summary>
+        /// <param ModelInput.Age="age">The first responders age id.</param>
+        /// <param ModelInput.Gender="sex">The first responders gender.</param>
+        /// <param Vitals="v">The first responders Vitals data.</param>
+
+
+        public static bool getDistressStatus(int age, 
+            char sex,
+            Vitals v
+            )
+        {
+            ModelInput FR = new ModelInput()
+            {
+                Age = age,
+                Gender = sex.ToString(),
+                HeartRate = v.HeartRate,
+                BloodOxy = v.BloodOxy,
+                Systolic_Blood_Pressure = v.SysBP,
+                Diastolic_Blood_Pressure = v.DiaBP,
+                Respiratory_Rate = v.RespRate,
+                Temperature = v.TempF
+            };
+
+            ModelOutput status = Distress.Predict(FR);
+            float chance = status.Probability;
+            bool distress = false;
+            if (chance >= 0.5)
+            {
+                distress = true;
+            }
+            else if (chance < 0.5)
+            {
+                distress = false;
+            }
+            /// @return [out] distress The distress status of a first responder.
+            return distress;
+        }
+    }
+
+
+}
--- MonitoringSuiteLibrary/MonitoringSuiteLibrary/MachineLearning/Distress.consumption.cs
+++ MonitoringSuiteLibrary/MonitoringSuiteLibrary/MachineLearning/Distress.consumption.cs
@@ -95,6 +95,7 @@ namespace MonitoringSuiteLibrary
 
         #endregion
 
+        //private static string MLNetModelPath = Path.GetFullPath("D:\\GitHubDesktopRepos\\CI-FirstResponders\\MonitoringSuiteLibrary\\MonitoringSuiteLibrary\\MachineLearning\\Distress.zip");
         private static string MLNetModelPath = Path.GetFullPath("Distress.zip");
 
         public static readonly Lazy<PredictionEngine<ModelInput, ModelOutput>> PredictEngine = new Lazy<PredictionEngine<ModelInput, ModelOutput>>(() => CreatePredictEngine(), true);
