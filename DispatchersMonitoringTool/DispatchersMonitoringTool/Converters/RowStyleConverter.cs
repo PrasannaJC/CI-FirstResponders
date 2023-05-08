377b7473ebf21a7c83a07b95f6ff39898247872b
Travis <tjmunyer@gmail.com>
2023-05-08 16:27:37 -0500

    Added documentation to a converter.

--- DispatchersMonitoringTool/DispatchersMonitoringTool/Converters/RowStyleConverter.cs
+++ DispatchersMonitoringTool/DispatchersMonitoringTool/Converters/RowStyleConverter.cs
@@ -9,8 +9,19 @@ using System.Windows.Data;
 
 namespace DispatchersMonitoringTool.Converters
 {
-    internal class RowStyleConverter : IValueConverter
+    /// <summary>
+    /// Converts a first responder alert to a color for UI representation.
+    /// </summary>
+    public class RowStyleConverter : IValueConverter
     {
+        /// <summary>
+        /// Converts first responder alert to a color.
+        /// </summary>
+        /// <param name="value">The value to convert.</param>
+        /// <param name="targetType">Required parameter for <see cref="IValueConverter"/>. Not used.</param>
+        /// <param name="parameter">Required parameter for <see cref="IValueConverter"/>. Not used.</param>
+        /// <param name="culture">Required parameter for <see cref="IValueConverter"/>. Not used.</param>
+        /// <returns>A string representing the color the alert was converted to.</returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if((value as FirstResponder).Alert == true)
@@ -27,6 +38,15 @@ namespace DispatchersMonitoringTool.Converters
             }
         }
 
+        /// <summary>
+        /// Not used.
+        /// </summary>
+        /// <param name="value">N/A</param>
+        /// <param name="targetType">N/A</param>
+        /// <param name="parameter">N/A</param>
+        /// <param name="culture">N/A</param>
+        /// <returns>N/A</returns>
+        /// <exception cref="NotImplementedException">Occurs when called.</exception>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
